0d4ce436107ac1403b78b8d6284b82e6e20ca757
Adriano Soares <gsoares.adriano@gmail.com>
2015-05-07 14:31:11 -0300

    Facebook asshole

---
+++ Quadradinho D8/Supporting Files/Facebook.m
@@ -0,0 +1,33 @@
+//
+//  Facebook.m
+//  Quadradinho D8
+//
+//  Created by Adriano Soares on 07/05/15.
+//  Copyright (c) 2015 Bepid. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NSString *FBTokenInformationExpirationDateKey = @"";
+NSString *FBTokenInformationTokenKey = @"";
+NSString *FBTokenInformationUserFBIDKey = @"";
+@interface FBAppCall:NSObject
+@end
+@implementation FBAppCall
+@end
+@interface FBRequest:NSObject
+@end
+@implementation FBRequest
+@end
+@interface FBSession:NSObject
+@end
+@implementation FBSession
+@end
+@interface FBSessionTokenCaching:NSObject
+@end
+@implementation FBSessionTokenCaching
+@end
+@interface FBSessionTokenCachingStrategy:NSObject
+@end
+@implementation FBSessionTokenCachingStrategy
+@end
