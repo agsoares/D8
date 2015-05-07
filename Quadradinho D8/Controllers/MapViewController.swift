db6f7855738441c0ff0674492d1a4d5ba50ab5b7
Adriano Soares <gsoares.adriano@gmail.com>
2015-05-07 16:52:54 -0300

    Constraint

--- Quadradinho D8/Controllers/MapViewController.swift
+++ Quadradinho D8/Controllers/MapViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import CoreLocation
 import MapKit
 
-class MapViewController: UIViewController, MKMapViewDelegate  {
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate  {
     @IBOutlet weak var map: MKMapView!
     var locationManager = CLLocationManager()
     
@@ -18,14 +18,47 @@ class MapViewController: UIViewController, MKMapViewDelegate  {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        locationManager.delegate = self
+        
+        if CLLocationManager.authorizationStatus() == .NotDetermined {
+            self.locationManager.requestAlwaysAuthorization()
+        }
+        
+
+        self.map.delegate = self
         // Do any additional setup after loading the view.
     }
 
+    
+    override func viewWillAppear(animated: Bool) {
+        var location = CLLocationCoordinate2D(latitude: 0, longitude: 0)
+        
+        if let userLocation = locationManager.location {
+            location = CLLocationCoordinate2D(
+                latitude: userLocation.coordinate.latitude,
+                longitude: userLocation.coordinate.longitude
+            )
+        }
+        
+        var span = MKCoordinateSpanMake(0.05, 0.05)
+        var region = MKCoordinateRegion(center: location, span: span)
+        
+        self.map.setRegion(region, animated: true)
+        self.map.showsUserLocation = true;
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if status == .AuthorizedAlways || status == .AuthorizedWhenInUse {
+            self.locationManager.startUpdatingLocation()
+        }
+    }
+
 
     /*
     // MARK: - Navigation
