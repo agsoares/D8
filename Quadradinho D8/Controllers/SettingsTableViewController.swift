0915043b899124ec7be5d20b245f192c1443152b
Lucas Flores <lucasflores77@gmail.com>
2015-05-12 14:59:22 -0300

    NEW STUFF MEGA POWER STORE

--- Quadradinho D8/Controllers/SettingsTableViewController.swift
+++ Quadradinho D8/Controllers/SettingsTableViewController.swift
@@ -9,123 +9,63 @@
 import UIKit
 import StoreKit
 
-class SettingsTableViewController: UITableViewController, SKStoreProductViewControllerDelegate {
-
+class SettingsTableViewController: UITableViewController, SKProductsRequestDelegate {
+    
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+//        
+//        
+//        let productsRequest = SKProductsRequest(productIdentifiers: ["produto"])
+//        productsRequest.delegate = self;
+//        productsRequest.start();
     }
-
+    
     override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+                super.didReceiveMemoryWarning()
+                // Dispose of any resources that can be recreated.
     }
     
     
-    @IBAction func showStoreView(sender: AnyObject) {
-        
-        let storeViewController = SKStoreProductViewController()
-        storeViewController.delegate = self
-        
-        let parameters = [SKStoreProductParameterITunesItemIdentifier :
-            NSNumber(integer: 676059878)]
-        
-        storeViewController.loadProductWithParameters(parameters,
-            completionBlock: {result, error in
-                if result {
-                   
-                    self.presentViewController(storeViewController,
-                        animated: true, completion: nil)
-                }
-                
-        })
-    }
+//    func productsRequest(request: SKProductsRequest!, didReceiveResponse response: SKProductsResponse!) {
+//        
+//    }
     
-    
-    func productViewControllerDidFinish(viewController:
-        SKStoreProductViewController!) {
-            viewController.dismissViewControllerAnimated(true,
-                completion: nil)
-    }
-    
-    func productsRequest(request: SKProductsRequest!, didReceiveResponse response: SKProductsResponse!) {
-        
-        var products = response.products
-        
-        if (products.count != 0) {
-            // Display the a “buy product” screen containing details
-            // from product object
-        }
-        
-        products = response.invalidProductIdentifiers
-        
-        for product in products
-        {
-            // Handle invalid product IDs if required
-        }
-    }
-    
-    func paymentQueue(queue: SKPaymentQueue!, updatedTransactions transactions: [AnyObject]!) {
-        
-        for transaction in transactions as! [SKPaymentTransaction] {
-            
-            switch transaction.transactionState {
-                
-            case SKPaymentTransactionState.Purchased:
-                if (transaction.downloads != nil) {
-                    SKPaymentQueue.defaultQueue().startDownloads(
-                        transaction.downloads)
-                } else {
-                    // Unlock feature or content here before
-                    // finishing transaction
-                    SKPaymentQueue.defaultQueue().finishTransaction(
-                        transaction)
-                }
-                
-            case SKPaymentTransactionState.Failed:
-                SKPaymentQueue.defaultQueue().finishTransaction(
-                    transaction)
-                
-            default:
-                break
-            }
-        }
-    }
-
-    @IBAction func premiumSelecter(sender: UISwitch) {
-        NSNotificationCenter.defaultCenter().postNotificationName ("transmitNotification", object:sender.on )
-    }
+//    @IBAction func premiumSelecter(sender: UISwitch) {
+//                NSNotificationCenter.defaultCenter().postNotificationName ("transmitNotification", object:sender.on )
+//                println(sender.on)
+    //}
     // MARK: - Table view data source
-
-//    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-//        // #warning Potentially incomplete method implementation.
-//        // Return the number of sections.
-//        return 0
-//    }
-//
-//    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        // #warning Incomplete method implementation.
-//        // Return the number of rows in the section.
-//        return 0
-//    }
-
+    
+    //    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    //        // #warning Potentially incomplete method implementation.
+    //        // Return the number of sections.
+    //        return 0
+    //    }
+    //
+    //    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    //        // #warning Incomplete method implementation.
+    //        // Return the number of rows in the section.
+    //        return 0
+    //    }
+    
     /*
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
-
-        // Configure the cell...
-
-        return cell
+    let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
+    
+    // Configure the cell...
+    
+    return cell
     }
     
-
-*/
+    
+    */
 }
