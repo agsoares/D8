c762ed130d155e55b5b934328087f67316fb4bf9
Lucas Flores <lucasflores77@gmail.com>
2015-05-12 22:45:31 -0300

    in app deliciosa 🍖

--- Quadradinho D8/Controllers/SettingsTableViewController.swift
+++ Quadradinho D8/Controllers/SettingsTableViewController.swift
@@ -9,63 +9,183 @@
 import UIKit
 import StoreKit
 
-class SettingsTableViewController: UITableViewController, SKProductsRequestDelegate {
+class SettingsTableViewController: UITableViewController, SKProductsRequestDelegate, SKPaymentTransactionObserver {
     
+    
+    
+    var product_id: NSString?;
     
     
     
     override func viewDidLoad() {
+        
+        product_id = "QD8";
+        
         super.viewDidLoad()
+        println("entroooou")
+        SKPaymentQueue.defaultQueue().addTransactionObserver(self)
         
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-        
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-//        
-//        
-//        let productsRequest = SKProductsRequest(productIdentifiers: ["produto"])
-//        productsRequest.delegate = self;
-//        productsRequest.start();
-    }
-    
-    override func didReceiveMemoryWarning() {
-                super.didReceiveMemoryWarning()
-                // Dispose of any resources that can be recreated.
-    }
-    
-    
-    func productsRequest(request: SKProductsRequest!, didReceiveResponse response: SKProductsResponse!) {
+        //addBotton();
         
     }
     
-//    @IBAction func premiumSelecter(sender: UISwitch) {
-//                NSNotificationCenter.defaultCenter().postNotificationName ("transmitNotification", object:sender.on )
-//                println(sender.on)
-    //}
-    // MARK: - Table view data source
     
-    //    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-    //        // #warning Potentially incomplete method implementation.
-    //        // Return the number of sections.
-    //        return 0
-    //    }
-    //
-    //    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    //        // #warning Incomplete method implementation.
-    //        // Return the number of rows in the section.
-    //        return 0
-    //    }
-    
-    /*
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
-    
-    // Configure the cell...
-    
-    return cell
+    @IBAction func buyNowButton(sender: AnyObject) {
+        
+        buyConsumable()
+        
     }
     
     
-    */
+    
+    
+    func buyConsumable(){
+        
+        println("About to fetch the products");
+        
+        // We check that we are allow to make the purchase.
+        
+        if (SKPaymentQueue.canMakePayments())
+            
+        {
+            
+            var productID:NSSet = NSSet(object: self.product_id!);
+            
+            var productsRequest:SKProductsRequest = SKProductsRequest(productIdentifiers: productID as Set<NSObject>);
+            
+            productsRequest.delegate = self;
+            
+            productsRequest.start();
+            
+            println("Fething Products");
+            
+        }else{
+            
+            println("can't make purchases");
+            
+        }
+        
+    }
+    
+    
+    
+    // Helper Methods
+    
+    
+    
+    func buyProduct(product: SKProduct){
+        
+        println("Sending the Payment Request to Apple");
+        
+        var payment = SKPayment(product: product)
+        
+        SKPaymentQueue.defaultQueue().addPayment(payment);
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    // Delegate Methods for IAP
+    
+    
+    
+    func productsRequest (request: SKProductsRequest, didReceiveResponse response: SKProductsResponse) {
+        
+        println("got the request from Apple")
+        
+        var count : Int = response.products.count
+        
+        if (count>0) {
+            
+            var validProducts = response.products
+            
+            var validProduct: SKProduct = response.products[0] as! SKProduct
+            
+            if (validProduct.productIdentifier == self.product_id) {
+                
+                println(validProduct.localizedTitle)
+                
+                println(validProduct.localizedDescription)
+                
+                println(validProduct.price)
+                
+                buyProduct(validProduct);
+                
+            } else {
+                
+                println(validProduct.productIdentifier)
+                
+            }
+            
+        } else {
+            
+            println("nothing")
+            
+        }
+        
+    }
+    
+    
+    
+    
+    
+    func request(request: SKRequest!, didFailWithError error: NSError!) {
+        
+        println("La vaina fallo");
+        
+    }
+    
+    
+    
+    func paymentQueue(queue: SKPaymentQueue!, updatedTransactions transactions: [AnyObject]!)    {
+        
+        println("Received Payment Transaction Response from Apple");
+        
+        
+        
+        for transaction:AnyObject in transactions {
+            
+            if let trans:SKPaymentTransaction = transaction as? SKPaymentTransaction{
+                
+                switch trans.transactionState {
+                    
+                case .Purchased:
+                    
+                    println("Product Purchased");
+                    
+                    SKPaymentQueue.defaultQueue().finishTransaction(transaction as! SKPaymentTransaction)
+                    
+                    break;
+                    
+                case .Failed:
+                    
+                    println("Purchased Failed");
+                    
+                    SKPaymentQueue.defaultQueue().finishTransaction(transaction as! SKPaymentTransaction)
+                    
+                    break;
+                    
+                    // case .Restored:
+                    
+                    //[self restoreTransaction:transaction];
+                    
+                default:
+                    
+                    break;
+                    
+                }
+                
+            }
+            
+        }
+        
+        
+        
+    }
+    
+
 }
